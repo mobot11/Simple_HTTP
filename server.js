3791d4d466451dbedb60435527ca189cb55ac770
Dylan T. Moberg <dylanmoberg@msn.com>
2015-07-16 00:00:16 -0700

    working on patch

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var express = require("express");
 var bodyParser = require("body-parser");
 var app = express();
 var fs = require('fs');
+var fileNameGenerator = require("./fileNameGenerator");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -44,11 +45,8 @@ app.get("/user:name", function(req,res) {
 app.post('/user/userPost:name',function(req,res){
 	var userData = req.body;
 	var name = req.params.name;
-	console.log(userData);
-	console.log(name);
 	var file = name.slice(1,name.length);
 	var fileName = __dirname + '/userData/' + file + '.json';
-	console.log(fileName);
 	fs.exists(fileName, function(exists) {
 		if(exists) {
 			fs.appendFile(fileName, JSON.stringify(userData), function(err) {
@@ -64,12 +62,42 @@ app.post('/user/userPost:name',function(req,res){
   res.end("yes");
 });
 
+app.delete('/user/userDelete:name',function(req, res) {
+	var name = req.params.name;
+	var file = name.slice(1,name.length);
+	var fileName = __dirname + '/userData/' + file + '.json';
+	fs.unlink(fileName, function(err) {
+		if(err) throw err;
+		console.log(file + ' successfully deleted!');
+	});
+});
 
 app.listen(port,function(){
   console.log("Started on PORT " + port);
+});
+
+app.patch('/user/userPatch:name', function(req, res) {
+	var name = req.params.name;
+	var file = name.slice(1,name.length);
+	var newData = req.body;
+	var fileName = __dirname + '/userData/' + file + '.json';
+	console.log(fileName);
+	fs.readFile(fileName, function(err,data) {
+		if(err) throw err;
+		var fileData = JSON.parse(data.toString());
+		console.log(fileData);
+		var patchData = fileData;
+		console.log(newData);
+		for(key in patchData) {			
+			if(patchData[key] !== newData[key]) {
+				patchData[key] === newData[key];
+			}
+			console.log(patchData);
+		}
+		console.log(patchData);
+
+	});
 })
-
-
 
 
 
